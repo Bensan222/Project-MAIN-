3535b24e8d49699bba75434e9c2c8fd43b1279eb
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-08-15 19:45:40 +0100

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"],
+  variable: "--font-inter"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`min-h-screen bg-background font-sans antialiased ${inter.className}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
